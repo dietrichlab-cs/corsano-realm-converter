b54d7f6229798689620cb20f2b7b35b2ac90e1ed
Luca Leon Happel <luca.happel@hhu.de>
2023-11-08 18:19:42 +0100

    fix: update schema-version to 210

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const Realm = require("realm");
-const dbm = require('./database-model');
-const fs = require('fs');
+const dbm = require("./database-model");
+const fs = require("fs");
 
 async function main() {
   if (process.argv.length != 4) {
     console.log("Usage: npm run start <realm-file> <output-dir>");
-    console.log("Output will be written to <output-dir>/MetricPpg.json, <output-dir>/StepsSummaryModel.json, etc.");
-    return
+    console.log(
+      "Output will be written to <output-dir>/MetricPpg.json, <output-dir>/StepsSummaryModel.json, etc.",
+    );
+    return;
   }
 
   const outputDir = process.argv[3];
@@ -19,16 +21,19 @@ async function main() {
   let realm = await Realm.open(realmConfig(process.argv[2]));
 
   // Go through all Objects in the realm file and convert them to JSON
-  for (const name of Object.keys(dbm).map(v => dbm[v].name)) {
+  for (const name of Object.keys(dbm).map((v) => dbm[v].name)) {
     console.log(name);
-    await fs.promises.appendFile(`${outputDir}/${name}.json`, JSON.stringify(realm.objects(name)))
+    await fs.promises.appendFile(
+      `${outputDir}/${name}.json`,
+      JSON.stringify(realm.objects(name)),
+    );
   }
 }
 
 const realmConfig = (path) => ({
   path: process.argv[2],
   schema: Object.keys(dbm).map((key) => dbm[key]),
-  schemaVersion: 5,
-})
+  schemaVersion: 210,
+});
 
-main()
+main();
