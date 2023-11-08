577be3cb07c3a6e8633bd420e41a6e87298b2624
Luca Leon Happel <luca.happel@hhu.de>
2023-11-08 19:30:37 +0100

    fix: use by default the android-schema

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const Realm = require("realm");
-const dbm = require("./database-model");
+const androidSchema = require("./android_realm_schema");
 const fs = require("fs");
 
 async function main() {
   if (process.argv.length != 4) {
     console.log("Usage: npm run start <realm-file> <output-dir>");
     console.log(
-      "Output will be written to <output-dir>/MetricPpg.json, <output-dir>/StepsSummaryModel.json, etc.",
+      "Output will be written to <output-dir>/MetricPpg.json, <output-dir>/StepsSummaryModel.json, etc."
     );
     return;
   }
@@ -21,18 +21,20 @@ async function main() {
   let realm = await Realm.open(realmConfig(process.argv[2]));
 
   // Go through all Objects in the realm file and convert them to JSON
-  for (const name of Object.keys(dbm).map((v) => dbm[v].name)) {
+  for (const name of Object.keys(androidSchema).map(
+    (v) => androidSchema[v].name
+  )) {
     console.log(name);
     await fs.promises.appendFile(
       `${outputDir}/${name}.json`,
-      JSON.stringify(realm.objects(name)),
+      JSON.stringify(realm.objects(name))
     );
   }
 }
 
 const realmConfig = (path) => ({
   path: process.argv[2],
-  schema: Object.keys(dbm).map((key) => dbm[key]),
+  schema: Object.keys(androidSchema).map((key) => androidSchema[key]),
   schemaVersion: 210,
 });
 
